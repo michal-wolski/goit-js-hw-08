c8585b8e036a187894d1a9605625a731c29c4017
michal-wolski <michal.wolski130@gmail.com>
2022-03-14 21:51:12 +0100

    tast 1 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryBox = document.querySelector("div.gallery");
+
+for (const el of galleryItems) {
+
+
+  const galleryLink = document.createElement("a");
+  galleryLink.classList.add("gallery__link");
+  galleryLink.href = el.original;
+
+  const galleryImage = document.createElement("img");
+  galleryImage.classList.add("gallery__image");
+  galleryImage.src = el.preview;
+  galleryImage.alt = el.description;
+  galleryBox.append(galleryLink);
+  galleryLink.append(galleryImage);
+};
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
