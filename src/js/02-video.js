764d0d399784a010245e0e57eede2a25dd984427
michal-wolski <michal.wolski130@gmail.com>
2022-03-14 23:02:13 +0100

    tast 2 and 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,59 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+// // player.getCurrentTime().then(function(sec) {
+// //     console.log(sec);
+// // })
+
+// let x = player.getCurrentTime().then(function(seconds) {
+//     return seconds
+// }).catch(function(error) {
+//     // an error occurred
+// });
+// player.setCurrentTime(40.456).then(function(seconds) {
+//     // seconds = the actual time that the player seeked to
+// }).catch(function(error) {
+//     switch (error.name) {
+//         case 'RangeError':
+//             // the time was less than 0 or greater than the video’s duration
+//             break;
+
+//         default:
+//             // some other error occurred
+//             break;
+//     }
+// });
+
+// console.log(x)
+
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+
+const timeUpdater = data => {
+  localStorage.setItem('videoplayer-current-time', JSON.stringify(data.seconds));
+};
+
+const lastTime = localStorage.getItem('videoplayer-current-time');
+const parsedLastTime = JSON.parse(lastTime);
+
+player.on('timeupdate', throttle(timeUpdater, 1000));
+
+console.log(data)
+
+player
+  .setCurrentTime(parsedLastTime)
+  .then(function (seconds) {
+  })
+  .catch(function (error) {
+    switch (error.name) {
+      case 'RangeError':
+        break;
+
+      default:
+        break;
+    }
+  });
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,19 @@
+import throttle from "lodash.throttle";
 
+const form = document.querySelector('form')
+const email = document.getElementsByName('email')
+const message = document.getElementsByName('message')
+const lable = document.querySelectorAll()
+
+
+
+
+
+
+
+
+
+
+
+
+console.log(lable);
