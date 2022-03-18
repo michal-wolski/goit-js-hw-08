b9d0673aa56ce68a088bc3e6c2082c62e3cfbbfc
michal-wolski <michal.wolski130@gmail.com>
2022-03-18 23:23:03 +0100

    testing

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,19 +1,29 @@
 import throttle from "lodash.throttle";
 
-const form = document.querySelector('form')
+
+const form = document.querySelector('.feedback-form')
 const email = document.getElementsByName('email')
 const message = document.getElementsByName('message')
-const lable = document.querySelectorAll()
 
 
 
+// form.addEventListener('input', throttle(formLisiner, 500));
 
+form.addEventListener("input", (ev) => {
+    ev.preventDefault();
+    const {
+        elements: {email, message}
+    } = form;
+    localStorage.setItem('feedback-form-state', JSON.stringify({email: email.value,message: message.value}))
+    
+});
 
+console.log(form.email.value);
+// const formLisiner = (ev) => {
+//     ev.preventDefault();
+//     const {
+//         elements: {email, message}
+//     } = ev.currentTarget;
+//     localStorage.setItem('feedback-form-state', JSON.stringify({email: email.value,message: message.value}))
+// }
 
-
-
-
-
-
-
-console.log(lable);
